eaed609a50b70b372a734720458ca941c7532f3e
WakoG13 <wakog13@moonhard.com>
2025-04-22 13:28:51 +0300

    other_ignore

--- other/push.cmd
+++
@@ -1,5 +0,0 @@
-cd C:\Users\skranis\Desktop\moonhard
-git add .
-git commit -m "Push full project folder with latest updates"
-git push
-pause
--- other/test_download.py
+++
@@ -1,7 +0,0 @@
-from program_downloader import ProgramDownloader
-
-if __name__ == "__main__":
-    print("🔧 Starting manual test of ProgramDownloader...")
-    pd = ProgramDownloader()
-    pd.download_programs(["Amvrosia Service"])
-    print("✅ Test completed.")
